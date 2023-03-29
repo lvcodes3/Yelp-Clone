ba42faa16c083863ed47e7dd51e7778e92e73d36
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-03-28 22:21:45 -0700

    simple additions

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config();
+
+const port = process.env.PORT;
 app.listen(5000, () => {
-    console.log("Server listening on PORT 5000");
+    console.log(`Server listening on PORT ${port}`);
 });
