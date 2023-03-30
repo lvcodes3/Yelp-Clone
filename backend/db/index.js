e8478b7f92c7915a17d3a9cec28433da3c154fe9
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-03-30 16:46:03 -0700

    fixed bug

--- backend/db/index.js
+++ backend/db/index.js
@@ -5,11 +5,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.DBPORT
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 module.exports = {
