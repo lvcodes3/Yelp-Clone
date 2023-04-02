10ff3e7d651155d3ac7bb9bf43033684eb7f00b1
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-04-02 16:02:11 -0700

    Reviews

--- frontend/src/components/Reviews.js
+++ frontend/src/components/Reviews.js
@@ -6,6 +6,7 @@ const Reviews = ({ reviews }) => {
       {reviews.map((review) => {
         return (
           <div
+            key={review.id}
             className="card text-white bg-primary mb-3 mr-4"
             style={{ width: "30%" }}
           >
@@ -21,50 +22,6 @@ const Reviews = ({ reviews }) => {
           </div>
         );
       })}
-      {/*
-      <div
-        className="card text-white bg-primary mb-3 mr-4"
-        style={{ width: "30%" }}
-      >
-        <div className="card-header d-flex justify-content-between">
-          <span className="h5">Luis</span>
-          <span className="h6">
-            <StarRating rating={2.5} />
-          </span>
-        </div>
-        <div className="card-body">
-          <p className="card-text h6">Awesome food!</p>
-        </div>
-      </div>
-      <div
-        className="card text-white bg-primary mb-3 mr-4"
-        style={{ width: "30%" }}
-      >
-        <div className="card-header d-flex justify-content-between">
-          <span className="h5">Luis</span>
-          <span className="h6">
-            <StarRating rating={2.5} />
-          </span>
-        </div>
-        <div className="card-body">
-          <p className="card-text h6">Awesome food!</p>
-        </div>
-      </div>
-      <div
-        className="card text-white bg-primary mb-3 mr-4"
-        style={{ width: "30%" }}
-      >
-        <div className="card-header d-flex justify-content-between">
-          <span className="h5">Luis</span>
-          <span className="h6">
-            <StarRating rating={2.5} />
-          </span>
-        </div>
-        <div className="card-body">
-          <p className="card-text h6">Awesome food!</p>
-        </div>
-      </div>
-  */}
     </div>
   );
 };
